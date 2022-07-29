0b16e997b4d87d8af86105e4a988a56dd2dfef5a
silica47 <techie-human@protonmail.com>
2022-07-29 20:49:02 +0530

    ban/unban

--- main.py
+++ main.py
@@ -29,6 +29,31 @@ async def eight_ball(ctx, *, question):
 
 @client.command()
 async def clear(ctx, amount=1):
-	await.ctx.channel.purge(limit=amount)
+	await ctx.channel.purge(limit=amount)
+	await ctx.send(f'{amount} messages have been deleted.')
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+	await member.kick(reason=reason)
+	await ctx.send(f'{member} have been kicked from the server.\nReason: {reason}')
+
+# ban command
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+	await member.ban(reason=reason)
+	await ctx.send(f'{member} have been banned from the server.\nReason: {reason}')
+
+@client.command()
+async def unban(ctx, *, member):
+	banned_users = await ctx.guild.bans()
+	member_name, member_discriminator = member.split('#')
+
+	for ban_entry in banned_users:
+		user = ban_entry.user
+
+		if (user.name, user.discriminator) == (member_name, member_discriminator):
+			await ctx.guild.unban(user)
+			await ctx.send(f'{user.name} have been unbanned from the server.')
+			return
 
 client.run('MTAwMDAzODY4OTI0MzY2ODYyMA.GDXrrR.pxABXtCL69LjKkrC8Pu8Sgo3WHpTxktGj9ccw8')
